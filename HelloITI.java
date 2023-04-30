0eb29df6be632a08228d12aa830314bf84154bea
Abd-Elrahman Esam <abdofahd20000@gmail.com>
2023-04-30 13:32:37 +0300

    helloITI with print git commit

---
+++ HelloITI.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+class Hello{  
+    public static void main(String args[]){  
+         helloPrintGit();
+    }
+
+
+	void helloPrintGit()
+	{
+	
+	   System.out.println("Hello Git");  
+	
+	}	
+} 
